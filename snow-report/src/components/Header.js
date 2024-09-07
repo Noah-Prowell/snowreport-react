57f4b8b64a0770128e9ec5a10f00ea93ae6e7ec8
Noah Prowell <noahprowell@gmail.com>
2024-09-07 10:59:52 -0600

    moved graph params to header

--- snow-report/src/components/Header.js
+++ snow-report/src/components/Header.js
@@ -1,8 +1,17 @@
 // src/components/Header.js
 import React from 'react';
-// import Dropdown from './components/Dropdown';
+import Dropdown from './Dropdown';
 
-const Header = () => {
+const Header = ({ 
+  stationOptions, 
+  selectedStationLabel, 
+  startDate, 
+  endDate, 
+  setStartDate, 
+  setEndDate, 
+  handleStationSelect, 
+  handleUpdateClick 
+}) => {
   
   return (
     <section id="header">
@@ -10,7 +19,37 @@ const Header = () => {
         <h1 id="logo"><a href="index.html">Snow Report</a></h1>
         <p>A site built by winter athletes for winter athletes</p>
         <br />
+        <h1 id="logo">{selectedStationLabel}</h1>
+        <br />
+        {/* Station Select Dropdown */}
+        <Dropdown 
+          label="Select Station:" 
+          options={stationOptions} 
+          onSelect={handleStationSelect} 
+        />
 
+        {/* Start Date Input */}
+        <div>
+          <label>Start Date: </label>
+          <input 
+            type="date" 
+            value={startDate} 
+            onChange={(e) => setStartDate(e.target.value)} 
+          />
+        </div>
+        
+        {/* End Date Input */}
+        <div>
+          <label>End Date: </label>
+          <input 
+            type="date" 
+            value={endDate} 
+            onChange={(e) => setEndDate(e.target.value)} 
+          />
+        </div>
+
+        {/* Update Button */}
+        <button onClick={handleUpdateClick}>Update Graph</button>
       </div>
     </section>
   );
